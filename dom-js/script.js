1fe8f912c7699b53611483a0a78e7619e2fd33eb
anjalii13 <anjuu1304@gmail.com>
2026-03-26 13:27:56 +0530

    dom

---
+++ dom-js/script.js
@@ -0,0 +1,27 @@
+function white(){
+
+    document.getElementById("badge").style.backgroundColor = "white";
+    document.getElementById("badge").style.color = "black";
+    document.getElementById("add-to-cart").style.backgroundColor = "white";
+    document.getElementById("car-image").src="assets/whiteCar.avif";
+
+}
+function gray(){
+
+    document.getElementById("badge").style.backgroundColor = "gray";
+    document.getElementById("add-to-cart").style.backgroundColor = "gray";
+    document.getElementById("car-image").src="assets/grayCar.avif";
+
+}
+function blue(){
+
+    document.getElementById("badge").style.backgroundColor = "rgb(4, 4, 104)";
+    document.getElementById("badge").style.Color = "white";
+
+    document.getElementById("add-to-cart").style.backgroundColor = "rgb(4, 4, 104)";
+    document.getElementById("cart").style.color = "white";
+    document.getElementById("add-to-cart").style.color = "white";
+    
+    document.getElementById("car-image").src="assets/blueCar.avif";
+
+}
